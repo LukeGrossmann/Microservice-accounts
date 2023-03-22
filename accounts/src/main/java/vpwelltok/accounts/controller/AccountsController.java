57c95f552177557214a0a3f6ff144cf6ee727dfa
Luke Grossmann <luke.grossmann@virginpulse.com>
2023-03-22 11:21:57 -0700

    Setup basic struc, controller, repos, model, h2 embedded sql

---
+++ accounts/src/main/java/vpwelltok/accounts/controller/AccountsController.java
@@ -0,0 +1,29 @@
+package vpwelltok.accounts.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import vpwelltok.accounts.model.Accounts;
+import vpwelltok.accounts.model.Customer;
+import vpwelltok.accounts.repository.AccountsRepository;
+@RestController
+public class AccountsController {
+
+    @Autowired
+    private AccountsRepository accountsRepository;
+
+    @PostMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestBody Customer customer) {
+
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        if (accounts != null) {
+            return accounts;
+        } else {
+            return null;
+        }
+
+    }
+
+}
---
+++ accounts/src/main/java/vpwelltok/accounts/repository/AccountsRepository.java
@@ -0,0 +1,13 @@
+package vpwelltok.accounts.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import vpwelltok.accounts.model.Accounts;
+
+@Repository
+public interface AccountsRepository extends CrudRepository<Accounts, Long> {
+
+    Accounts findByCustomerId(int customerId);
+
+}
